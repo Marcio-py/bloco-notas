25e06d0912799e4450099ff00b84d8d193f8633d
Marcio <marcio1961.ma@gmail.com>
2023-05-23 10:06:41 +0100

    add dynamic render

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Header from "../components/Header";
-import Note from "../components/Note"; 
+import Note from "../components/Note";
 import Footer from "../components/Footer";
+import notes from "../components/notes";
 
 function App() {
   return (
     <div>
       <Header />
       <Footer />
-      <Note />
+      {notes.map((noteItem) => {
+        return (
+          <Note
+            key={noteItem.key}
+            title={noteItem.title}
+            description={noteItem.description}
+          />
+        );
+      })}
     </div>
   );
 }
